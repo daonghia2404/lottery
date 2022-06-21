32778b828f4ee29a44140e8965263ec546db9fa6
daonghia2404 <daonghia2404@gmail.com>
2022-06-21 16:35:15 +0700

    Add function buy ticket

--- src/containers/BannerDailyLottery/BannerDailyLottery.jsx
+++ src/containers/BannerDailyLottery/BannerDailyLottery.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
 import ImageRibbon from '@/assets/images/image-ribbon.png';
 import ImageRibbonYellow from '@/assets/images/image-ribbon-yellow.png';
 import CountdownTimer from '@/components/CountdownTimer';
 import Button from '@/components/Button';
+import ModalLotteryResults from '@/containers/ModalLotteryResults';
+import ModalBuyLottery from '@/containers/ModalBuyLottery';
 
 import './BannerDailyLottery.scss';
-import classNames from 'classnames';
 
 const BannerDailyLottery = ({ price, title, subtitle, showCountdown, backgroundDaily }) => {
+  const [visibleLotteryResultsModal, setVisibleLotteryResultsModal] = useState(false);
+  const [visibleBuyLotteryModal, setVisibleBuyLotteryModal] = useState(false);
+
+  const handleOpenBuyLotteryModal = () => {
+    setVisibleBuyLotteryModal(true);
+  };
+  const handleCloseBuyLotteryModal = () => {
+    setVisibleBuyLotteryModal(false);
+  };
+
+  const handleOpenLotteryResultsModal = () => {
+    setVisibleLotteryResultsModal(true);
+  };
+  const handleCloseLotteryResultsModal = () => {
+    setVisibleLotteryResultsModal(false);
+  };
+
+  const handleBuyTickets = () => {
+    handleCloseBuyLotteryModal();
+    handleOpenLotteryResultsModal();
+  };
+
   return (
     <div className={classNames('BannerDailyLottery', { 'background-daily': backgroundDaily })}>
       <div className="container">
@@ -26,10 +50,14 @@ const BannerDailyLottery = ({ price, title, subtitle, showCountdown, backgroundD
           )}
 
           <div className="BannerDailyLottery-btn">
-            <Button className="yellow" title="BUY TICKET NOW" />
+            <Button className="yellow" title="BUY TICKET NOW" onClick={handleOpenBuyLotteryModal} />
           </div>
         </div>
       </div>
+
+      <ModalLotteryResults visible={visibleLotteryResultsModal} onClose={handleCloseLotteryResultsModal} />
+
+      <ModalBuyLottery visible={visibleBuyLotteryModal} onClose={handleCloseBuyLotteryModal} onBuy={handleBuyTickets} />
     </div>
   );
 };
